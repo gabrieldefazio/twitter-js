a05ae3d1bbc419aba3d319f59fdc0ff0443c073d
Gabriel Defazio <gabrieldefazio@Gabriels-MacBook-Air.local>
2017-09-19 15:45:45 -0400

    added more functionality

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,30 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+
 router.get('/', function (req, res) {
     let tweets = tweetBank.list();
-    res.render( 'index', { tweets: tweets } );
+    res.render( 'index', { tweets: tweets, showForm: true } );
 });
 
 router.get('/users/:name/', function(req, res) {
     var name = req.params.name;
     var list = tweetBank.find( {name: name} );
-    res.render( 'index', { tweets: list } );
+    res.render( 'index', { tweets: list, showForm: true } );
 });
 
 router.get('/users/:name/tweets/:id', function(req, res) {
     var id = req.params.id;
-    console.log(id)
     var list = tweetBank.find( {id: Number(id)} );
-    console.log(list)
     res.render( 'index', { tweets: list } );
 });
 
+router.post('/tweets', function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    var id = tweetBank.id();
+    tweetBank.add(name, text, id);
+    res.redirect('/');
+});
 
 module.exports = router;
